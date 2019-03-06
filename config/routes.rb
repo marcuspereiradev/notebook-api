b0355cb91a4b6e152992709496f864de285c06e0
marcuspereiragithub <vinnycoast@gmail.com>
2019-03-06 09:16:46 -0300

    Deserialization. Transform a json into a hash to interpret rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+  resources :contacts do
+    resource :kind, only: [ :show ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
