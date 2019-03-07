551457b32364cb4ec88f27e9731b1178a0785ab4
marcuspereiragithub <vinnycoast@gmail.com>
2019-03-07 16:44:37 -0300

    CRUD AMS with has_one(Address), has_many(Phones)

--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -1,3 +1,6 @@
 class PhoneSerializer < ActiveModel::Serializer
   attributes :id, :number
+  belongs_to :contact do
+    link(:related) { contact_phones_url(object.contact.id) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
     resource :phones, only: [ :show ]
     resource :phones, only: [ :show ], path: 'relationships/phones'
 
-    resource :address, only: [ :show ]
-    resource :address, only: [ :show ], path: 'relationships/address'
+    resource :phone, only: [ :update, :create, :destroy ]
+    resource :phone, only: [ :update, :create, :destroy ], path: 'relationships/phones'
+
+    resource :address, only: [ :show, :update, :create, :destroy ]
+    resource :address, only: [ :show, :update, :create, :destroy ], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
